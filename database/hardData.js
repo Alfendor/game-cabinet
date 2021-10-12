b23a9834cff3de8ed502cc07ecc53134f74423a8
Derek Samms <derek.samms@gmail.com>
2021-10-12 16:22:28 -0700

    added files, created larek-games database and began populating tables

---
+++ database/hardData.js
@@ -0,0 +1,13 @@
+var cabinet = [
+  {
+    id:
+    title: "Harry Potter: Hogwarts Battle"
+    minPlayers: 2,
+    maxPlayers: 4,
+    time: 60,
+    cooperative: true,
+    theme: [],
+    mechanics: ['Scenario, Mission, or Campaign', 'Variable Player Powers'],
+    equipment: ['Dice', 'Cards', 'Tokens', 'Board(s)']
+  }
+]
